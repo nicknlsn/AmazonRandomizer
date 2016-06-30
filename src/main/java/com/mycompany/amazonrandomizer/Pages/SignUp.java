1382e300e8798c099b01c2ec1fa9b3be44c3c0e0
Nick Nelson <nicknlsn@gmail.com>
2016-06-30 11:10:57 -0600

    refactored package names.

--- src/main/java/com/mycompany/amazonrandomizer/Pages/SignUp.java
+++ src/main/java/com/mycompany/amazonrandomizer/Pages/SignUp.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Pages;
+package com.mycompany.amazonrandomizer.Pages;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -17,7 +17,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import util.JDBCUtils;
+import com.mycompany.amazonrandomizer.util.JDBCUtils;
 
 /**
  *
--- src/main/java/com/mycompany/amazonrandomizer/util/JDBCUtils.java
+++ src/main/java/com/mycompany/amazonrandomizer/util/JDBCUtils.java
@@ -3,14 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package util;
+package com.mycompany.amazonrandomizer.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
